fc2156e3e4cb1bd8d971b794b40908c3d9f7e459
francisco-kpodotay <135797234+francisco-kpodotay@users.noreply.github.com>
2024-10-31 19:46:20 +0100

    task 1, 2, 3, 4 implemented

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./app.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+const App: React.FC =()=>{
+  return (
+   <>
+   <Header/>
+   <Main/>
+   </>
+  )
+}
+
+export default App;
