fcf62070311de8c25461e068e41602af589e463a
zsolt270 <zsolt270000@gmail.com>
2024-10-31 19:53:13 +0100

    valami

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-  </StrictMode>,
-)
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
