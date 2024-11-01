880edcd82db49a01e0c0ec20199fdc4ba0b14620
zsolt270 <zsolt270000@gmail.com>
2024-11-01 20:05:19 +0100

    updated the UI and also installed react-icons

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,24 +1,32 @@
 import { TodoType } from "./Main";
 
 type Props = {
-  todos: TodoType[];
-  doDelete: (text: string) => void;
+	todos: TodoType[];
+	doDelete: (text: string) => void;
 };
 
 const TodoList: React.FC<Props> = ({ todos, doDelete }) => {
-  return (
-    <>
-      {todos.map((todo) => {
-        return (
-          <div key={todo.text}>
-            <span>{todo.text}</span>
-            <span>{todo.isDone ? "Done" : "Not Done"}</span>
-            <button onClick={()=>doDelete(todo.text)}>Delete</button>
-          </div>
-        );
-      })}
-    </>
-  );
+	return (
+		<>
+			{todos.map((todo) => {
+				return (
+					<div
+						key={todo.text}
+						className='todoRows'
+					>
+						<span>{todo.isDone ? "Done" : "Not Done"}</span>
+						<span>{todo.text}</span>
+						<button
+							className='deleteBtn'
+							onClick={() => doDelete(todo.text)}
+						>
+							Delete
+						</button>
+					</div>
+				);
+			})}
+		</>
+	);
 };
 
 export default TodoList;
